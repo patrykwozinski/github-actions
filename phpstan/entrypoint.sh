2e00b8679d6cb441f8093c7c4b5320a0d9749985
Patryk Woziński <patryk.wozinski@docplanner.com>
2019-02-15 14:29:00 +0100

    variables in phpstan

--- phpstan/entrypoint.sh
+++ phpstan/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "composer install -n --prefer-dist --no-scripts -o && /composer/vendor/bin/phpstan analyse $*"
+sh -c "composer install -n --prefer-dist --no-scripts -o && /composer/vendor/bin/phpstan analyse $* $(git diff --name-only --diff-filter=MAd ${BRANCH}...HEAD -- ${FILTERS})"
