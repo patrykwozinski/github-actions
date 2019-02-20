7c1e397acd55991f2560f96f4a5a670f88062e8e
Patryk Woziński <patryk.wozinski@docplanner.com>
2019-02-20 10:50:13 +0100

    removed info about remotes

--- php-cs-fixer/entrypoint.sh
+++ php-cs-fixer/entrypoint.sh
@@ -7,6 +7,4 @@ echo "Analysed files:"
 echo "-----------------"
 echo ${FILES}
 
-git remote -v
-
 /usr/local/bin/php-cs-fixer fix $* ${FILES}
--- phpstan/entrypoint.sh
+++ phpstan/entrypoint.sh
@@ -7,6 +7,4 @@ echo "Analysed files:"
 echo "-----------------"
 echo ${FILES}
 
-git remote -v
-
 composer install -n --prefer-dist --no-scripts -o && /composer/vendor/bin/phpstan analyse $* ${FILES}
