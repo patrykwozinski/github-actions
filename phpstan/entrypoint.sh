0e58479a935bf202a72df7002cb50bda6bfff70b
Patryk Woziński <patryk.wozinski@gmail.com>
2019-03-18 14:45:37 +0100

    counting files

--- phpstan/entrypoint.sh
+++ phpstan/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-COMMAND="git diff-index --name-only --diff-filter=MAd HEAD~$(jq '.commits | length' "${GITHUB_EVENT_PATH}") ${DIRECTORY}"
+COMMAND="git diff-index --name-only --diff-filter=MAd HEAD~$(jq --raw-output .pull_request.commits "$GITHUB_EVENT_PATH") ${DIRECTORY}"
 
 echo 'Command:'
 echo ${COMMAND}
