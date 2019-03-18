0449d55b5b9e88cac23cba3f06138a044cc23701
Patryk Woziński <patryk.wozinski@gmail.com>
2019-03-18 13:10:37 +0100

    remove shitty code

--- list-changed-files/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-if ! git diff-index --quiet HEAD~$(jq '.commits | length' "${GITHUB_EVENT_PATH}") $*; then
-  echo "Changes in $*, proceeding"
-else
-  echo "No changes in $*, stopping" && echo "ignore:$*" >> "$HOME/ignore"
-fi
