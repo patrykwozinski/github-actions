c56b198f0ca69b5c710fff9877c837703a2bf2d4
Patryk Woziński <patryk.wozinski@gmail.com>
2019-03-18 15:55:33 +0100

    save changed files

---
+++ list-changed-files/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -l
+
+FILES=$(git diff-index --name-only --diff-filter=MAd HEAD~$(jq --raw-output .pull_request.commits "$GITHUB_EVENT_PATH") $DIRECTORY)
+
+if [[ -z $FILES ]]; then
+    echo 'No changed files'
+    exit 78
+fi
+
+echo $FILES > .ga-changed-files.txt
